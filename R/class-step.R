4957a8a65b5f6843777b519ba208634d0a6fbc9b
yun <yunyunpp96@outlook.com>
2022-12-31 03:05:04 +0800

    add `seed` for `step` object

--- R/class-step.R
+++ R/class-step.R
@@ -2,11 +2,15 @@
 # user-friendly helper
 #' Build a `step` object
 #' A `step` object define the command to run in the pipeline.
+#' @description
+#' steps define the command the run in the analysis pipeline
+#'  - step: user-friendly helper
+#'  - new_step: low-level constructor
 #' @param call The command to run. Notes: this will be enclosed by
 #' <[`rlang::enquo()`]> to bundle the environment of users. If you use the step
-#' in another environment other than the current environment. You should quote
-#' the expression firstly then use <[`!!`][rlang::injection-operator]> to inject
-#' the call expression.
+#' in another environment other than the current environment. You should use
+#' [new_step] or quote the expression firstly then use
+#' <[`!!`][rlang::injection-operator]> to inject the call expression in [step].
 #' @param deps A character vector defines the upstream steps to run before
 #' runing this step.
 #' @param finished A scalar `logical` indicates whether this step has been
@@ -16,21 +20,29 @@
 #' evaluated from `call` won't be saved. Otherwise, the results will be saved.
 #' if `TRUE` or `NULL`, the result name will be the same with the name in
 #' `step_tree` object. Or a sclar `character` define the name.
+#' @param seed  A scalar `logical`, `numeric`, or `NULL`. a `logical` value
+#' indicates whether to set seed when evaluated the command in "call". if
+#' `TRUE`, the call is evaluated with a seed (based on the hash of the call
+#' object). if `numeric`, seed will be set by `set.seed(as.integer(seed))`.
+#' Otherwise, the call is evaluated without seed.
 #' @return A `step` object.
 #' @name step
-#' @export 
-step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
+#' @export
+step <- function(call, deps = NULL, finished = FALSE, return = NULL, seed = NULL) {
     new_step(
         call = rlang::enquo(call),
         deps = deps,
         finished = finished,
-        return = return
+        return = return,
+        seed = seed
     )
 }
 
 # low-level constructor
 # steps define the command the run in the analysis pipeline
-new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
+#' @export
+#' @rdname step
+new_step <- function(call, deps = NULL, finished = FALSE, return = NULL, seed = NULL) {
     if (!rlang::is_call(call)) {
         cli::cli_abort("{.arg call} should be a {.cls call} object")
     }
@@ -41,28 +53,35 @@ new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
         cli::cli_abort("{.arg finished} should be a scalar {.cls logical}")
     }
     if (!(is.null(return) || rlang::is_scalar_character(return) || rlang::is_scalar_logical(return))) {
-        # nolint
         cli::cli_abort("{.arg return} should be a scalar {.cls character} or {.cls character}, or {.val NULL}")
+    }
+    if (isTRUE(seed)) {
+        seed <- digest::digest2int(digest::digest(call, "crc32"), seed = 0L)
+    } else if (rlang::is_scalar_double(seed) || rlang::is_scalar_integer(seed)) {
+        seed <- as.integer(seed)
+    } else if (!(is.null(seed) || isFALSE(seed))) {
+        cli::cli_abort("{.arg seed} should be a scalar {.cls logical} or {.cls numeric}, or {.val NULL}")
     }
     structure(
         list(
             call = call, deps = deps,
-            finished = finished, return = return
+            finished = finished, return = return,
+            seed = seed
         ),
         class = "step"
     )
 }
 
-#' @export 
+#' @export
 #' @param x A `step` object from which to extract element(s) or in which to
-#' replace element(s). 
+#' replace element(s).
 #' @param name The indices specifying elements to extract or replace
 #' @rdname step
 `[[.step` <- function(x, name) {
     NextMethod()
 }
 
-#' @export 
+#' @export
 #' @rdname step
 `[[<-.step` <- function(x, name, value) {
     step <- NextMethod()
@@ -72,35 +91,37 @@ new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
     )
 }
 
-#' @export 
+#' @export
 #' @rdname step
 `$.step` <- function(x, name) {
     NextMethod()
 }
 
-#' @export 
+#' @export
 #' @rdname step
 `$<-.step` <- function(x, name, value) {
     step <- NextMethod()
     step(
         call = step$call, deps = step$deps,
-        finished = step$finished, return = step$return
+        finished = step$finished, return = step$return,
+        seed = step$seed
     )
 }
 
-#' @export 
+#' @export
 #' @rdname step
 `[.step` <- function(x, name) {
     NextMethod()
 }
 
-#' @export 
+#' @export
 #' @rdname step
 `[<-.step` <- function(x, name, value) {
     step <- NextMethod()
     step(
         call = step$call, deps = step$deps,
-        finished = step$finished, return = step$return
+        finished = step$finished, return = step$return,
+        seed = step$seed
     )
 }
 
@@ -108,7 +129,7 @@ new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
 #' Reports whether x is an `step` object
 #' @param x An object to test
 #' @keywords internal
-#' @noRd 
+#' @noRd
 is_step <- function(x) inherits(x, "step")
 
 #' Run step call command
@@ -118,19 +139,44 @@ is_step <- function(x) inherits(x, "step")
 #' have priority over those in `envir`.
 #' @param envir The environment in which to evaluate the `call` in step.
 #' @keywords internal
-#' @noRd 
+#' @noRd
 run_step <- function(step, mask = NULL, envir = rlang::caller_env()) {
+    if (rlang::is_scalar_integer(step$seed)) {
+        old_seed <- rlang::env_get(globalenv(), ".Random.seed", default = NULL)
+        if (is.null(old_seed)) {
+            on.exit(rm(".Random.seed", envir = globalenv()))
+        } else {
+            on.exit(rlang::env_bind(globalenv(), .Random.seed = old_seed))
+        }
+        set.seed(step$seed)
+    }
     mask <- rlang::new_data_mask(mask)
     mask$.data <- rlang::as_data_pronoun(mask)
     rlang::eval_tidy(step$call, data = mask, env = envir)
 }
 
+assert_step <- function(step) {
+    if (!is_step(step)) {
+        cli::cli_abort(c(
+            "{.var step} must be a {.cls step} object",
+            "x" = "You've supplied a {.cls {class(step)}}."
+        ))
+    }
+}
+
 reset_step <- function(step) {
+    assert_step(step)
     step$finished <- FALSE
     step
 }
 
 finish_step <- function(step) {
+    assert_step(step)
     step$finished <- TRUE
     step
+}
+
+is_finished <- function(step) {
+    assert_step(step)
+    isTRUE(step$finished)
 }

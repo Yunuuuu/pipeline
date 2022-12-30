b0c2e312e647540d49c00f12dadbb7468f35f175
yun <yunyunpp96@outlook.com>
2022-12-30 14:54:32 +0800

    complete the document

--- R/class-step.R
+++ R/class-step.R
@@ -1,17 +1,49 @@
 # step class
 # user-friendly helper
+#' Build a `step` object
+#' A `step` object define the command to run in the pipeline.
+#' @param call The command to run. Notes: this will be enclosed by
+#' <[`rlang::enquo()`]> to bundle the environment of users. If you use the step
+#' in another environment other than the current environment. You should quote
+#' the expression firstly then use <[`!!`][rlang::injection-operator]> to inject
+#' the call expression.
+#' @param deps A character vector defines the upstream steps to run before
+#' runing this step.
+#' @param finished A scalar `logical` indicates whether this step has been
+#' evaluated.
+#' @param return A scalar `logical`, `character`, or `NULL`. a `logical` value
+#' indicates whether to keep the returned value. if `FALSE`, the result
+#' evaluated from `call` won't be saved. Otherwise, the results will be saved.
+#' if `TRUE` or `NULL`, the result name will be the same with the name in
+#' `step_tree` object. Or a sclar `character` define the name.
+#' @return A `step` object.
+#' @name step
+#' @export 
 step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
-    validate_step(new_step(
+    new_step(
         call = rlang::enquo(call),
         deps = deps,
         finished = finished,
         return = return
-    ))
+    )
 }
 
 # low-level constructor
 # steps define the command the run in the analysis pipeline
 new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
+    if (!rlang::is_call(call)) {
+        cli::cli_abort("{.arg call} should be a {.cls call} object")
+    }
+    if (!(is.null(deps) || rlang::is_character(deps))) {
+        cli::cli_abort("{.arg deps} should be a scalar {.cls character} or {.val NULL}")
+    }
+    if (!rlang::is_scalar_logical(finished)) {
+        cli::cli_abort("{.arg finished} should be a scalar {.cls logical}")
+    }
+    if (!(is.null(return) || rlang::is_scalar_character(return) || rlang::is_scalar_logical(return))) {
+        # nolint
+        cli::cli_abort("{.arg return} should be a scalar {.cls character} or {.cls character}, or {.val NULL}")
+    }
     structure(
         list(
             call = call, deps = deps,
@@ -21,24 +53,17 @@ new_step <- function(call, deps = NULL, finished = FALSE, return = NULL) {
     )
 }
 
-validate_step <- function(x) {
-    values <- unclass(x)
-    if (!(rlang::is_character(values$deps) || is.null(values$deps))) {
-        cli::cli_abort("{.arg deps} should be a scalar {.cls character} or {.val NULL}")
-    }
-    if (!rlang::is_scalar_logical(values$finished)) {
-        cli::cli_abort("{.arg finished} should be a scalar {.cls logical}")
-    }
-    if (!(rlang::is_scalar_character(values$return) || is.null(values$return))) {
-        cli::cli_abort("{.arg return} should be a scalar {.cls character} or {.val NULL}")
-    }
-    x
-}
-
+#' @export 
+#' @param x A `step` object from which to extract element(s) or in which to
+#' replace element(s). 
+#' @param name The indices specifying elements to extract or replace
+#' @rdname step
 `[[.step` <- function(x, name) {
     NextMethod()
 }
 
+#' @export 
+#' @rdname step
 `[[<-.step` <- function(x, name, value) {
     step <- NextMethod()
     step(
@@ -47,9 +72,14 @@ validate_step <- function(x) {
     )
 }
 
+#' @export 
+#' @rdname step
 `$.step` <- function(x, name) {
     NextMethod()
 }
+
+#' @export 
+#' @rdname step
 `$<-.step` <- function(x, name, value) {
     step <- NextMethod()
     step(
@@ -58,10 +88,14 @@ validate_step <- function(x) {
     )
 }
 
+#' @export 
+#' @rdname step
 `[.step` <- function(x, name) {
     NextMethod()
 }
 
+#' @export 
+#' @rdname step
 `[<-.step` <- function(x, name, value) {
     step <- NextMethod()
     step(
@@ -74,16 +108,29 @@ validate_step <- function(x) {
 #' Reports whether x is an `step` object
 #' @param x An object to test
 #' @keywords internal
+#' @noRd 
 is_step <- function(x) inherits(x, "step")
 
-#' Reports whether x is an `step` object
+#' Run step call command
+#' Run step in a mask environment.
 #' @param step A step object.
 #' @param mask The environment used to implement data mask, Objects in `mask`
 #' have priority over those in `envir`.
 #' @param envir The environment in which to evaluate the `call` in step.
 #' @keywords internal
+#' @noRd 
 run_step <- function(step, mask = NULL, envir = rlang::caller_env()) {
     mask <- rlang::new_data_mask(mask)
     mask$.data <- rlang::as_data_pronoun(mask)
     rlang::eval_tidy(step$call, data = mask, env = envir)
+}
+
+reset_step <- function(step) {
+    step$finished <- FALSE
+    step
+}
+
+finish_step <- function(step) {
+    step$finished <- TRUE
+    step
 }

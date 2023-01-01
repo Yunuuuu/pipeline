b7c64c0f377e35421c309234108d674fa824b0a6
yun <yunyunpp96@outlook.com>
2023-01-01 14:48:14 +0800

    add `assert_class` helper

--- R/utils.R
+++ R/utils.R
@@ -14,6 +14,21 @@ has_names <- function(x) {
     !is.na(nms) & nms != ""
 }
 
+#' Report if an argument is a specific class
+#' @keywords internal
+#' @noRd
+assert_class <- function(x, class, arg = rlang::caller_arg(x), call = rlang::caller_env()) {
+    if (!inherits(x, class)) {
+        cli::cli_abort(
+            c(
+                "{.arg {arg}} must be a {.cls {class}} object",
+                "x" = "You've supplied a {.cls {class(x)}}."
+            ),
+            call = call
+        )
+    }
+}
+
 #' implement similar purrr::imap function
 #' @note this function won't keep the names of .x in the final result
 #' @keywords internal

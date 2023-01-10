e83e103838e927977f921f5baa5b13061c824e5d
yun <yunyunpp96@outlook.com>
2023-01-10 17:18:06 +0800

    add custom_step class

---
+++ R/class-custom_step.R
@@ -0,0 +1,14 @@
+step_unbind <- function(nms, deps = NULL) {
+    new_custom_step(
+        id = "unbind", call = NULL, 
+        deps = deps, finished = FALSE, return = FALSE, seed = FALSE
+    )
+}
+
+new_custom_step <- function(...) {
+    step <- new_step(...)
+    class(step) <- c("custom_step", class(step))
+    step
+}
+
+is_custom_step <- function(x) inherits(x, "custom_step")

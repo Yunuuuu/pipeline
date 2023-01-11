719fe46e90bec97e102e59a6ad7eecfcd7fd81fe
yun <yunyunpp96@outlook.com>
2023-01-12 03:59:24 +0800

    fix R CMD check notes

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,13 @@
 if (getRversion() >= "2.15.1") {
     utils::globalVariables(
-        c( # `cellmarker_prepare`
+        c(
+            "self", # used to specify R6 class
+            # `cellmarker_prepare`
             "gene_list",
             # Variable used in function `cellmarker_search`
             "targeted", "gene_list", "targeted_size", "targeted_prop",
             # variable used in function `step_sce_normalize`
-            "main", "sizefactor"
+             "sizefactor"
         )
     )
 }

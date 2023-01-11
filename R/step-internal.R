bd2a428fca9ce2d24422bc24c38e06e32e97ae78
yun <yunyunpp96@outlook.com>
2023-01-12 03:53:03 +0800

    fix error: no unbind method

--- R/step-internal.R
+++ R/step-internal.R
@@ -2,7 +2,7 @@ step_unbind <- function(nms, deps = NULL) {
     nms <- rlang::enquo(nms)
     new_step(
         id = "unbind",
-        call = rlang::expr(self$unbind(nms = !!nms)),
+        call = rlang::expr(self$env_unbind(nms = !!nms)),
         deps = deps, finished = FALSE, return = FALSE, seed = FALSE
     )
 }
